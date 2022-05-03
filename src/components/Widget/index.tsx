f6176ceef6251fd0651cecbdd32707789f5e652d
Kiérico Souza <kiericograndao@gmail.com>
2022-05-03 14:48:20 -0400

    Conceito de Acessibilidade - Utilizando Popover

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,24 +1,29 @@
 import { ChatTeardropDots } from 'phosphor-react';
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
     return (
-        <div className="absolute bottom-4 right-4">
-            <button className="
-                bg-brand-500 
+        <Popover className="absolute bottom-4 right-4">
+            <Popover.Panel>Hello User!</Popover.Panel>
+            <Popover.Button className="bg-brand-500 
                 rounded-full 
                 px-3 
                 h-12 
                 text-[#ffffff]
                 flex 
                 items-center 
-                group
-                ">
+                group">
                 <ChatTeardropDots className="w-6 h-6" />
-                <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+                <span className="max-w-0 
+                    overflow-hidden 
+                    group-hover:max-w-xs 
+                    transition-all 
+                    duration-500 
+                    ease-linear">
                     <span className='pl-2'></span>
                     Feedback
                 </span>
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     )
 }
